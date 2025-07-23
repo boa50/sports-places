5a3818fa9ae84d55a2f4f926aca40a90975bf8f8
boa50 <bruno.albuquerque50@gmail.com>
2025-07-23 15:35:42 -0300

    Created the index map page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+    ZoomControl,
+} from 'react-leaflet'
 
 export const Route = createFileRoute('/')({
     component: Index,
@@ -6,9 +13,24 @@ export const Route = createFileRoute('/')({
 
 function Index() {
     return (
-        <div className="p-2">
-            <h3>Welcome Homes!</h3>
-            <div className="bg-red-500">tests</div>
-        </div>
+        <MapContainer
+            id="root"
+            className="h-dvh"
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={true}
+            zoomControl={false}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+            <ZoomControl position="bottomright" />
+        </MapContainer>
     )
 }
