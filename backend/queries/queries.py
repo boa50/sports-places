43866ac827123fed6177cc08500bd7f681051650
boa50 <bruno.albuquerque50@gmail.com>
2025-07-24 17:31:26 -0300

    Created the function to read users' reviews

--- backend/queries/queries.py
+++ backend/queries/queries.py
@@ -2,6 +2,7 @@ def get_reviews_data():
     data = [
         [51.505, -0.09, "Review 1 <br/> Some test"],
         [51.550, -0.09, "Review 2"],
+        [-15.7515776, -47.8937088, "Home Review 3"],
     ]
 
     return data
--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -5,12 +5,13 @@ import {
     ScaleControl,
 } from 'react-leaflet'
 import { latLngBounds, latLng } from 'leaflet'
-import RecenterMapButton from '../components/RecenterMapButton'
 import { defaults } from './defaults'
-import Review from './Review'
 
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { reviewsQueryOptions } from '../queryOptions/reviews'
+
+import Review from '../components/Review'
+import RecenterMapButton from '../components/RecenterMapButton'
 
 interface Props {
     id: string
@@ -19,9 +20,6 @@ interface Props {
 
 export default function Map({ id, userLocation }: Props) {
     const maxBounds = latLngBounds(latLng(-90, -Infinity), latLng(90, Infinity))
-
-    const reviewsQuery = useSuspenseQuery(reviewsQueryOptions)
-    const reviews = reviewsQuery.data
 
     return (
         <MapContainer
@@ -38,18 +36,35 @@ export default function Map({ id, userLocation }: Props) {
             maxBounds={maxBounds}
             maxBoundsViscosity={1.0}
         >
+            {getComponents()}
+        </MapContainer>
+    )
+}
+
+function getComponents() {
+    return (
+        <>
+            {getReviews()}
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-
-            {reviews.map((review, i) => (
-                <Review key={i} review={review} />
-            ))}
-
             <ScaleControl position="bottomleft" />
             <RecenterMapButton />
             <ZoomControl position="bottomright" />
-        </MapContainer>
+        </>
+    )
+}
+
+function getReviews() {
+    const reviewsQuery = useSuspenseQuery(reviewsQueryOptions)
+    const reviews = reviewsQuery.data
+
+    return (
+        <>
+            {reviews.map((review, i) => (
+                <Review key={i} review={review} />
+            ))}
+        </>
     )
 }
