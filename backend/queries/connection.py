839d8906a7d583a91c68ccc01eb79d8be8fd9225
boa50 <bruno.albuquerque50@gmail.com>
2025-07-25 16:07:36 -0300

    Created a sample code to connect to the database

---
+++ backend/queries/connection.py
@@ -0,0 +1,43 @@
+import psycopg2
+import os
+
+
+def execute_query(sql):
+    try:
+        conn = psycopg2.connect(
+            database=os.environ.get("PGDATABASE"),
+            user=os.environ.get("PGUSER"),
+            password=os.environ.get("PGPASSWORD"),
+            host=os.environ.get("PGHOST"),
+            port=os.environ.get("PGPORT"),
+        )
+
+        # Create a cursor object to execute SQL queries
+        cur = conn.cursor()
+
+        rows = None
+        column_names = None
+
+        cur.execute(sql)
+
+        if sql[:6] == "SELECT":
+            # Fetch data
+            rows = cur.fetchall()
+
+            if cur.description is not None:
+                column_names = [desc[0] for desc in cur.description]
+        else:
+            # Commit data
+            conn.commit()
+
+    except psycopg2.Error as e:
+        print(f"Error connecting to PostgreSQL: {e}")
+
+    finally:
+        # Close the cursor and connection
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+        return rows, column_names
--- backend/queries/queries.py
+++ backend/queries/queries.py
@@ -1,8 +1,19 @@
-def get_reviews_data():
-    data = [
-        [51.505, -0.09, "Review 1 <br/> Some test"],
-        [51.550, -0.09, "Review 2"],
-        [-15.7515776, -47.8937088, "Home Review 3"],
-    ]
+import pandas as pd
+from dotenv import load_dotenv
+from connection import execute_query
 
-    return data
+
+load_dotenv()
+
+
+def get_reviews_data():
+    sql = "SELECT * FROM Reviews;"
+
+    data, column_names = execute_query(sql)
+
+    df = pd.DataFrame(data, columns=column_names)
+
+    return df
+
+
+print(get_reviews_data())
