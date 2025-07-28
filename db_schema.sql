91cbab1c73cc237dde0dfcf6964a1267c12ef180
boa50 <bruno.albuquerque50@gmail.com>
2025-07-28 17:53:22 -0300

    Create a file to store the database schema

---
+++ db_schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users  (
+    user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS places  (
+    place_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    lat FLOAT,
+    lng FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS reviews  (
+    review_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    place_id INT NOT NULL,
+    rating INT,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (place_id) REFERENCES places (place_id) ON DELETE CASCADE
+);
