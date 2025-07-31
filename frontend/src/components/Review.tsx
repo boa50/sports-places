600896c48569dd36c240d0a373ebe80b0a063c0b
boa50 <bruno.albuquerque50@gmail.com>
2025-07-31 18:10:52 -0300

    Started styling the reviews card

--- frontend/src/components/Review.tsx
+++ frontend/src/components/Review.tsx
@@ -1,11 +1,16 @@
 export default function Review() {
     return (
-        <div className="text-sm font-medium text-gray-900">
-            <p>User Name</p>
-            <p>User Title</p>
-            <p>User reviews punctuation</p>
-            <p>Date (how many years/months/days from now)</p>
-            <p>Rating</p>
+        <div className="flex flex-col text-sm font-sans font-normal">
+            <div className="flex flex-col leading-3">
+                <div className="text-base">User Name</div>
+                <div className="text-gray-500">
+                    User Title - User reviews punctuation
+                </div>
+            </div>
+            <div className="flex gap-x-2 pt-2">
+                <div>Rating</div>
+                <div>Date (how many years/months/days from now)</div>
+            </div>
         </div>
     )
 }
