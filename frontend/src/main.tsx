2c25a002c088cc3aba224bb01bdcbbe5f9bd73a7
boa50 <bruno.albuquerque50@gmail.com>
2025-08-14 17:58:57 -0300

    Created the configuration to use the firebase auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,22 @@ import { routeTree } from './routeTree.gen.ts'
 import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
 
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+// Firebase Auth Config
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
+
+const app = initializeApp(firebaseConfig)
+getAuth(app)
+
 const queryClient = new QueryClient()
 
 // Create a new router instance
