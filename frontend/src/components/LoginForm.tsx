31c1d9e006a1dd84e9eda1d8b60d5a2b6cdce985
boa50 <bruno.albuquerque50@gmail.com>
2025-08-19 14:50:17 -0300

    Made the login form more dynamic

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useAppContext } from '@/contexts/AppContext'
 import { Button, FormModal, Input, Link } from './ui'
 
@@ -6,51 +6,192 @@ export default function LoginForm() {
     const { state, dispatch } = useAppContext()
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
+    const [screen, setScreen] = useState<
+        'signIn' | 'signUp' | 'forgotPassword'
+    >('signIn')
+    const [formTitle, setFormTitle] = useState<string>(
+        'Sign in to your account'
+    )
 
-    const handleCloseModal = () => {
-        dispatch({ type: 'HIDE_LOGIN_FORM' })
+    const resetFields = () => {
         setEmail('')
         setPassword('')
     }
 
+    useEffect(() => {
+        resetFields()
+
+        switch (screen) {
+            case 'signIn':
+                setFormTitle('Sign in to your account')
+                break
+            case 'signUp':
+                setFormTitle('Create a new account')
+                break
+            case 'forgotPassword':
+                setFormTitle('Reset your password')
+                break
+
+            default:
+                setFormTitle('Sign in to your account')
+                break
+        }
+    }, [screen])
+
+    const handleCloseModal = () => {
+        dispatch({ type: 'HIDE_LOGIN_FORM' })
+        setScreen('signIn')
+        resetFields()
+    }
+
     return (
         <FormModal
-            title="Sign in to your account"
+            title={formTitle}
             isModalOpen={state.isLoginFormOpen}
             closeModal={handleCloseModal}
         >
-            <form className="space-y-6" action="#">
-                <Input
-                    id="email"
-                    label="Email"
-                    type="email"
-                    placeholder="name@company.com"
-                    hint="Fill with your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    isFullWidth={true}
+            {screen === 'signIn' && (
+                <SignInContent
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
+                    setScreen={setScreen}
                 />
-                <Input
-                    id="password"
-                    label="Password"
-                    type="password"
-                    placeholder="••••••••"
-                    hint="Fill with your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    isFullWidth={true}
+            )}
+            {screen === 'signUp' && (
+                <SignUpContent
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
                 />
-                <div className="flex justify-end">
-                    <Link title="Forgot password?" url="#" />
-                </div>
-                <Button title="Sign in" width="w-full" isSubmit={true} />
-                <div className="flex gap-1">
-                    <span className="text-sm font-light text-gray-900">
-                        Don’t have an account yet?
-                    </span>
-                    <Link title="Sign up" url="#" />
-                </div>
-            </form>
+            )}
+            {screen === 'forgotPassword' && (
+                <ForgotPasswordContent email={email} setEmail={setEmail} />
+            )}
         </FormModal>
+    )
+}
+
+interface SignInContentProps {
+    email: string
+    setEmail: React.Dispatch<React.SetStateAction<string>>
+    password: string
+    setPassword: React.Dispatch<React.SetStateAction<string>>
+    setScreen: React.Dispatch<
+        React.SetStateAction<'signIn' | 'signUp' | 'forgotPassword'>
+    >
+}
+
+function SignInContent({
+    email,
+    setEmail,
+    password,
+    setPassword,
+    setScreen,
+}: SignInContentProps) {
+    return (
+        <form className="space-y-6" action="#">
+            <Input
+                id="email"
+                label="Email"
+                type="email"
+                placeholder="name@company.com"
+                hint="Fill with your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                isFullWidth={true}
+            />
+            <Input
+                id="password"
+                label="Password"
+                type="password"
+                placeholder="••••••••"
+                hint="Fill with your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                isFullWidth={true}
+            />
+            <div className="flex justify-end">
+                <Link
+                    title="Forgot password?"
+                    onClick={() => setScreen('forgotPassword')}
+                />
+            </div>
+            <Button title="Sign In" width="w-full" isSubmit={true} />
+            <div className="flex gap-1">
+                <span className="text-sm font-light text-gray-900">
+                    Don’t have an account yet?
+                </span>
+                <Link title="Sign up" onClick={() => setScreen('signUp')} />
+            </div>
+        </form>
+    )
+}
+
+interface SignUpContentProps {
+    email: string
+    setEmail: React.Dispatch<React.SetStateAction<string>>
+    password: string
+    setPassword: React.Dispatch<React.SetStateAction<string>>
+}
+
+function SignUpContent({
+    email,
+    setEmail,
+    password,
+    setPassword,
+}: SignUpContentProps) {
+    return (
+        <form className="space-y-6" action="#">
+            <Input
+                id="email"
+                label="Email"
+                type="email"
+                placeholder="name@company.com"
+                hint="Fill with your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                isFullWidth={true}
+            />
+            <Input
+                id="password"
+                label="Password"
+                type="password"
+                placeholder="••••••••"
+                hint="Fill with your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                isFullWidth={true}
+            />
+            <Button title="Sign Up" width="w-full" isSubmit={true} />
+        </form>
+    )
+}
+
+interface ForgotPasswordContentProps {
+    email: string
+    setEmail: React.Dispatch<React.SetStateAction<string>>
+}
+
+function ForgotPasswordContent({
+    email,
+    setEmail,
+}: ForgotPasswordContentProps) {
+    return (
+        <form className="space-y-6" action="#">
+            <Input
+                id="email"
+                label="Email"
+                type="email"
+                placeholder="name@company.com"
+                hint="Fill with your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                isFullWidth={true}
+            />
+            <Button title="Reset Password" width="w-full" isSubmit={true} />
+        </form>
     )
 }
--- frontend/src/components/ui/Link.tsx
+++ frontend/src/components/ui/Link.tsx
@@ -1,18 +1,30 @@
 interface Props {
     title: string
-    url: string
+    url?: string
     isOpenNewTab?: boolean
+    onClick?: () => void
 }
 
-export function Link({ title, url, isOpenNewTab = false }: Props) {
-    return (
+export function Link({ title, url, isOpenNewTab = false, onClick }: Props) {
+    const defaultClass = 'text-sm font-medium text-sky-700 hover:underline'
+
+    return url !== undefined ? (
         <a
             href={url}
             target={isOpenNewTab ? '_blank' : '_top'}
-            className="text-sm font-medium text-sky-700 hover:underline"
+            className={defaultClass}
             title={url}
         >
             {title}
         </a>
+    ) : (
+        <div
+            role="button"
+            className={`${defaultClass} cursor-pointer`}
+            title={title}
+            onClick={onClick}
+        >
+            {title}
+        </div>
     )
 }
