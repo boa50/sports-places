bb1b666657a2f9d7e6da1118384c117eb1d68674
boa50 <bruno.albuquerque50@gmail.com>
2025-09-15 16:28:52 -0300

    Created tests for the alert ui component

---
+++ frontend/src/components/ui/Alert.test.tsx
@@ -0,0 +1,26 @@
+import { screen, render } from '@testing-library/react'
+import { Alert } from './Alert'
+
+test('show success alert', () => {
+    const message = 'some success message'
+    render(<Alert message={message} type="success" />)
+
+    expect(screen.getByRole('alert')).toHaveClass('text-sky-700 bg-sky-50/90')
+    expect(screen.getByRole('alert')).toHaveTextContent(message)
+})
+
+test('show error alert', () => {
+    const message = 'a different error alert'
+    render(<Alert message={message} type="error" />)
+
+    expect(screen.getByRole('alert')).toHaveClass('text-red-700 bg-red-50/90')
+    expect(screen.getByRole('alert')).toHaveTextContent(message)
+})
+
+test('show info alert', () => {
+    const message = 'just a simple info'
+    render(<Alert message={message} type="info" />)
+
+    expect(screen.getByRole('alert')).toHaveClass('text-gray-700 bg-gray-50/90')
+    expect(screen.getByRole('alert')).toHaveTextContent(message)
+})
